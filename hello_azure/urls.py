c61729a00520261a2911389121f87c2fcd5493d8
EfrainDiazM <efradiaz28@gmail.com>
2023-11-20 16:47:19 -0500

    subir fotos storage

--- hello_azure/urls.py
+++ hello_azure/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('hello', views.hello, name='hello'),
+    path('upload_image/', views.upload_image, name='upload_image'),
 ]
--- hello_azure/views.py
+++ hello_azure/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from azure.storage.blob import BlobServiceClient, ContentSettings
+
+ACCOUNT_NAME = 'pruebaaedm'
+ACCOUNT_KEY = '97Bl6LBNWT4TYYc4H1tTvG57O4jQUj2ED9i13SVZU+mCPbBlf98NIRsWlcXabTi5eJEIDA+xBtU3+AStMJ59UQ=='
+CONTAINER_NAME = 'prueba'
 
 def index(request):
     print('Request for index page received')
@@ -22,3 +27,28 @@ def hello(request):
             return render(request, 'hello_azure/hello.html', context)
     else:
         return redirect('index')
+    
+def get_blob_service_client():
+    connection_string = f"DefaultEndpointsProtocol=https;AccountName={ACCOUNT_NAME};AccountKey={ACCOUNT_KEY};EndpointSuffix=core.windows.net"
+    return BlobServiceClient.from_connection_string(connection_string)
+
+def upload_image_to_blob_storage(image_data, blob_name):
+    blob_service_client = get_blob_service_client()
+    container_client = blob_service_client.get_container_client(CONTAINER_NAME)
+
+    blob_client = container_client.get_blob_client(blob_name)
+    blob_client.upload_blob(image_data, content_settings=ContentSettings(content_type='image/png'))
+
+@csrf_exempt
+def upload_image(request):
+    if request.method == 'POST' and request.FILES['image']:
+        image_file = request.FILES['image']
+        blob_name = image_file.name
+
+        image_data = image_file.read()
+
+        upload_image_to_blob_storage(image_data, blob_name)
+
+        return HttpResponse("Imagen cargada exitosamente")
+    else:
+        return HttpResponse("La solicitud debe ser de tipo POST y contener un archivo adjunto")
